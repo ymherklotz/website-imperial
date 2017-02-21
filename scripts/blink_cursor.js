a2bcfc4711cc7c39487968f62745001e1a4e66b2
Yann Herklotz <ymherklotz@gmail.com>
2017-02-21 17:22:13 +0000

    Making layout better

---
+++ scripts/blink_cursor.js
@@ -0,0 +1,7 @@
+setInterval(function() {
+    if(document.getElementById("cursor").style.visibility == "hidden") {
+	document.getElementById("cursor").style.visibility = "visible";
+    } else {
+	document.getElementById("cursor").style.visibility = "hidden";
+    }
+}, 500);
